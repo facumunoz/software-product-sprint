c344a3af3fbb6a730a9f3680a17b5de18901bb6d
Facundo <facumunozo@gmail.com>
2021-03-03 04:58:27 +0000

    Add fetch() function to display message

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,11 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function fetchMessage() {
+    const responseFromServer = await fetch('/hello');
+    const textFromResponse = await responseFromServer.text();
+    
+    const messageContainer = document.getElementById('message-container');
+    messageContainer.innerText = textFromResponse;
+}
